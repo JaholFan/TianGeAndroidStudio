0487cb9c998e4824ed4bf2f37f292b456a40247a
jahol <1028795881@qq.com>
2020-08-22 09:04:41 +0800

    2.3_Button

---
+++ 2.3_Button/app/src/main/java/com/example/helloworld/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button mBtnTextView;
+    private Button mBtnButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mBtnTextView = findViewById(R.id.btn_textview);
+        mBtnTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //跳转到TextViewActivity
+                Intent intent =new Intent(MainActivity.this,TextViewActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        mBtnButton = findViewById(R.id.btn_button);
+        mBtnButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //跳转到Button演示界面
+                Intent intent = new Intent(MainActivity.this,ButtonActivity.class);
+                startActivity(intent);
+    }
+});
+    }
+}
